aee9c4b85cb8ffc724be0d90891aebeeea7e29d8
James Thorniley <james.thorniley@gmail.com>
2022-07-03 14:37:21 +0100

    Avoid buffer copy

--- examples/create_image.rs
+++ examples/create_image.rs
@@ -2,16 +2,18 @@ use image::save_buffer;
 use perlinrs::perlin_cube;
 
 pub fn main() {
-    let mut result = vec![0; 20 * 20 * 20 * 20];
+    let mut result = vec![0; 10 * 40 * 10 * 40];
     for i in 0..10usize {
         for j in 0..10usize {
-            let data = perlin_cube(i as i16, j as i16, 40, 40);
-            for k in 0..40 {
-                for l in 0..40 {
-                    let in_value = data[k * 40 + l];
-                    result[(i * 16000 + k * 400 + j * 40 + l)] = in_value;
-                }
-            }
+            perlin_cube(
+                i as i16,
+                j as i16,
+                40,
+                40,
+                &mut result,
+                i * 40 + j * 16000,
+                400,
+            );
         }
     }
     save_buffer("./output.png", &result, 400, 400, image::ColorType::L8)
--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,8 @@ fn corner_vector(x: i16, y: i16) -> Vec2 {
     let mut hasher = DefaultHasher::new();
     hasher.write_i16(x);
     hasher.write_i16(y);
-    let hash_value = hasher.finish();
-    let direction = hash_value as f32 / u64::MAX as f32;
+    let hash_value = hasher.finish() as u8;
+    let direction = hash_value as f32 / u8::MAX as f32;
     Vec2::from_direction(direction * std::f32::consts::PI * 2.0)
 }
 
@@ -33,14 +33,21 @@ fn interpolate(a0: f32, a1: f32, w: f32) -> f32 {
     (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0
 }
 
-pub fn perlin_cube(x: i16, y: i16, nx: u16, ny: u16) -> Vec<u8> {
+pub fn perlin_cube(
+    x: i16,
+    y: i16,
+    nx: u16,
+    ny: u16,
+    buf: &mut Vec<u8>,
+    offset: usize,
+    stride: usize,
+) {
     let c1 = corner_vector(x, y);
     let c2 = corner_vector(x, y + 1);
     let c3 = corner_vector(x + 1, y);
     let c4 = corner_vector(x + 1, y + 1);
 
-    let mut buf = vec![0u8; (nx * ny).into()];
-    let mut bufIdx = 0usize;
+    let mut buf_idx = offset;
 
     let dx = 1.0f32 / nx as f32;
     let dy = 1.0f32 / ny as f32;
@@ -48,8 +55,8 @@ pub fn perlin_cube(x: i16, y: i16, nx: u16, ny: u16) -> Vec<u8> {
     let mut x = dx / 2.0;
     let mut y = dy / 2.0;
 
-    for i in 0..nx {
-        for j in 0..ny {
+    for i in 0..ny {
+        for j in 0..nx {
             let p1 = Vec2::new(-x, -y).dot(&c1);
             let p2 = Vec2::new(-x, 1.0 - y).dot(&c2);
             let p3 = Vec2::new(1.0 - x, -y).dot(&c3);
@@ -59,15 +66,14 @@ pub fn perlin_cube(x: i16, y: i16, nx: u16, ny: u16) -> Vec<u8> {
             let i2 = interpolate(p3, p4, y);
             let i = interpolate(i1, i2, x);
 
-            buf[bufIdx] = (u8::MAX as f32 * (i / 2.0 + 0.5)) as u8;
-            y += dy;
-            bufIdx += 1;
+            buf[buf_idx] = (u8::MAX as f32 * (i / 2.0 + 0.5)) as u8;
+            x += dx;
+            buf_idx += 1;
         }
-        y = dy / 2.0;
-        x += dx;
+        buf_idx += stride - nx as usize;
+        x = dx / 2.0;
+        y += dy;
     }
-
-    return buf;
 }
 
 #[cfg(test)]
