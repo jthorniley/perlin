074ee993f2e49349f045be885f56e97f9ba351ee
James Thorniley <james.thorniley@gmail.com>
2022-07-10 09:51:21 +0100

    Tidy, fix edge effects

--- examples/create_image.rs
+++ examples/create_image.rs
@@ -5,20 +5,20 @@ use ndarray::Array;
 use perlinrs::AddPerlinNoise;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-    let w = 400;
-    let h = 200;
+    let cols = 400;
+    let rows = 200;
 
-    let mut img = Array::zeros([h, w]);
+    let mut img = Array::zeros([rows, cols]);
 
-    img.add_perlin_noise(13);
+    img.add_perlin_noise(15);
 
     let result = img.map(|value: &f32| ((value / 2.0 + 0.5).clamp(0.0, 1.0) * 255.0) as u8);
 
     save_buffer(
         "./output.png",
         result.as_slice().ok_or("Unexpected error")?,
-        w as u32,
-        h as u32,
+        cols as u32,
+        rows as u32,
         image::ColorType::L8,
     )?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{num::Wrapping, ops::Range};
+use std::num::Wrapping;
 
 use ndarray::{prelude::*, DataMut};
 
@@ -17,6 +17,7 @@ impl Vec2 {
         Vec2 { x, y }
     }
 
+    /// Performs [-(self.x, self.y) + (a, b)] <dot> other
     fn negate_add_dot(&self, a: f32, b: f32, other: &Vec2) -> f32 {
         return (a - self.x) * other.x + (b - self.y) * other.y;
     }
@@ -37,14 +38,14 @@ fn hash(value: u32) -> u8 {
     keyed.rotate_right((value & 0xf) as u32) as u8
 }
 
-fn random_vector(x: u32, y: u32) -> Vec2 {
-    let hash_value = hash(x | y.rotate_left(16));
+fn random_vector(row_index: usize, col_index: usize) -> Vec2 {
+    let hash_value = hash(row_index as u32 | (col_index as u32).rotate_left(16));
     let direction = hash_value as f32 / 255.0;
     Vec2::from_direction(direction * std::f32::consts::PI * 2.0)
 }
 
 pub trait Perlin {
-    fn perlin_inplace(&mut self, x: u32, y: u32);
+    fn perlin_inplace(&mut self, row_index: usize, col_index: usize, scale: usize);
 }
 
 impl<E, S> Perlin for ArrayBase<S, Dim<[usize; 2]>>
@@ -52,35 +53,33 @@ where
     E: From<f32> + Copy,
     S: DataMut<Elem = E>,
 {
-    fn perlin_inplace(&mut self, x: u32, y: u32) {
+    fn perlin_inplace(&mut self, row_index: usize, col_index: usize, scale: usize) {
         let corners = [
-            random_vector(x, y),
-            random_vector(x, y + 1),
-            random_vector(x + 1, y),
-            random_vector(x + 1, y + 1),
+            random_vector(row_index, col_index),
+            random_vector(row_index, col_index + 1),
+            random_vector(row_index + 1, col_index),
+            random_vector(row_index + 1, col_index + 1),
         ];
 
-        if let [nrows, ncols] = self.shape() {
-            let vert_pixel_size = 1.0 / *nrows as f32;
-            let horz_pixel_size = 1.0 / *ncols as f32;
+        let vert_pixel_size = 1.0 / scale as f32;
+        let horz_pixel_size = 1.0 / scale as f32;
 
-            self.indexed_iter_mut().for_each(|((i, j), val)| {
-                let pixel = Vec2::new(
-                    horz_pixel_size / 2.0 + horz_pixel_size * j as f32,
-                    vert_pixel_size / 2.0 + vert_pixel_size * i as f32,
-                );
-                let p1 = pixel.negate_add_dot(0.0, 0.0, &corners[0]);
-                let p2 = pixel.negate_add_dot(0.0, 1.0, &corners[1]);
-                let p3 = pixel.negate_add_dot(1.0, 0.0, &corners[2]);
-                let p4 = pixel.negate_add_dot(1.0, 1.0, &corners[3]);
+        self.indexed_iter_mut().for_each(|((i, j), val)| {
+            let pixel = Vec2::new(
+                horz_pixel_size / 2.0 + horz_pixel_size * j as f32,
+                vert_pixel_size / 2.0 + vert_pixel_size * i as f32,
+            );
+            let p1 = pixel.negate_add_dot(0.0, 0.0, &corners[0]);
+            let p2 = pixel.negate_add_dot(1.0, 0.0, &corners[1]);
+            let p3 = pixel.negate_add_dot(0.0, 1.0, &corners[2]);
+            let p4 = pixel.negate_add_dot(1.0, 1.0, &corners[3]);
 
-                let interp1 = interpolate(p1, p2, pixel.y);
-                let interp2 = interpolate(p3, p4, pixel.y);
-                let interp = interpolate(interp1, interp2, pixel.x);
+            let interp1 = interpolate(p1, p2, pixel.x);
+            let interp2 = interpolate(p3, p4, pixel.x);
+            let interp = interpolate(interp1, interp2, pixel.y);
 
-                *val = interp.into();
-            });
-        }
+            *val = interp.into();
+        });
     }
 }
 pub trait AddPerlinNoise {
@@ -97,90 +96,20 @@ where
     S: DataMut<Elem = E>,
 {
     fn add_perlin_noise(&mut self, scale: usize) {
-        if let [ny, nx] = self.shape() {
-            let (nx, ny) = (*nx, *ny);
-            let x_iter = IntervalIter::new(scale, nx);
+        if let [rows, cols] = self.shape() {
+            let (rows, cols) = (*rows, *cols);
 
-            for (x, x_range) in x_iter {
-                let y_iter = IntervalIter::new(scale, ny);
-                for (y, y_range) in y_iter {
-                    let slice_info = s![y_range, x_range.clone()];
-                    let mut slice = self.slice_mut(slice_info);
-                    slice.perlin_inplace(x as u32, y as u32);
+            for row_index in (0..rows).step_by(scale) {
+                for col_index in (0..cols).step_by(scale) {
+                    let row_end = rows.min(row_index + scale);
+                    let col_end = cols.min(col_index + scale);
+                    let mut slice = self.slice_mut(s![row_index..row_end, col_index..col_end]);
+                    slice.perlin_inplace(row_index / scale, col_index / scale, scale);
                 }
             }
         }
     }
 }
 
-struct IntervalIter {
-    n: u32,
-    pos: usize,
-    scale: usize,
-    len: usize,
-}
-
-impl IntervalIter {
-    fn new(scale: usize, len: usize) -> IntervalIter {
-        IntervalIter {
-            n: 0,
-            pos: 0,
-            scale,
-            len,
-        }
-    }
-}
-
-impl Iterator for IntervalIter {
-    type Item = (u32, Range<usize>);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let start = self.pos;
-        let end = (self.pos + self.scale).min(self.len);
-
-        if start >= end {
-            None
-        } else {
-            self.pos += self.scale;
-            self.n += 1;
-            Some((self.n - 1, Range { start, end }))
-        }
-    }
-}
-
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_interval_iter_behaviour() {
-        let interval_iter = IntervalIter::new(10, 25);
-        let collected: Vec<Range<usize>> = interval_iter.map(|(n, range)| range).collect();
-        assert_eq!(collected, vec![0..10, 10..20, 20..25]);
-    }
-
-    #[test]
-    fn test_interval_iter_behaviour_short() {
-        let interval_iter = IntervalIter::new(10, 2);
-        let collected: Vec<Range<usize>> = interval_iter.map(|(n, range)| range).collect();
-        assert_eq!(collected, vec![0..2]);
-    }
-    #[test]
-    fn test_interval_iter_behaviour_len_zero() {
-        let interval_iter = IntervalIter::new(10, 0);
-        let collected: Vec<Range<usize>> = interval_iter.map(|(n, range)| range).collect();
-        assert_eq!(collected, vec![]);
-    }
-    #[test]
-    fn test_interval_iter_behaviour_len_exact() {
-        let interval_iter = IntervalIter::new(4, 12);
-        let collected: Vec<Range<usize>> = interval_iter.map(|(n, range)| range).collect();
-        assert_eq!(collected, vec![0..4, 4..8, 8..12]);
-    }
-    #[test]
-    fn test_interval_iter_behaviour_scale_zero() {
-        let interval_iter = IntervalIter::new(0, 12);
-        let collected: Vec<Range<usize>> = interval_iter.map(|(n, range)| range).collect();
-        assert_eq!(collected, vec![]);
-    }
-}
+mod tests {}
