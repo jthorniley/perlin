90d87011c0e773fd556cb1d0400cb8b66878ed10
James Thorniley <james.thorniley@gmail.com>
2022-07-17 08:12:18 +0100

    Delete old code

--- perlinrs-web/src/lib.rs
+++ perlinrs-web/src/lib.rs
@@ -34,27 +34,3 @@ impl ImageGenerator {
         self.data.add_perlin_noise(scale, amp);
     }
 }
-
-/*
-#[wasm_bindgen]
-pub struct PerlinNoise {
-    data: Box<[u8]>,
-}
-
-#[wasm_bindgen]
-impl PerlinNoise {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> PerlinNoise {
-        let mut data = Box::new([0; 100]);
-        data[0] = 12;
-        PerlinNoise { data }
-    }
-
-    pub fn get_data(&self) -> JsValue {
-        let m: Memory = memory().unchecked_into();
-        let begin = self.data.as_ptr();
-        let slice = Uint8Array::new(&m.buffer());
-        slice.slice(begin as u32, begin as u32 + 100).into()
-    }
-}
-*/
