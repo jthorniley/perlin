316a88a8151e5834e2338d9c4d86a4fd72e57eba
James Thorniley <james.thorniley@gmail.com>
2022-07-26 21:53:05 +0100

    Create an rgba image model in wasm

--- imtools/src/wasm.rs
+++ imtools/src/wasm.rs
@@ -1,6 +1,7 @@
-use js_sys::{Float32Array, WebAssembly::Memory};
-use ndarray::{Array, Dim};
-use wasm_bindgen::{memory, prelude::*, JsCast};
+use js_sys::{Float32Array, Uint8Array, Uint8ClampedArray, WebAssembly::Memory};
+use ndarray::prelude::*;
+use wasm_bindgen::{memory, prelude::*, Clamped, JsCast};
+use web_sys::ImageData;
 
 #[wasm_bindgen]
 pub struct ScalarImage {
@@ -28,22 +29,53 @@ impl ScalarImage {
     }
 }
 
+#[wasm_bindgen]
+pub struct RgbaImage {
+    data: Array2<[u8; 4]>,
+}
+
+#[wasm_bindgen]
+impl RgbaImage {
+    pub fn fill(width: usize, height: usize, r: u8, g: u8, b: u8) -> RgbaImage {
+        let data = Array2::from_elem((height, width), [r, g, b, 255]);
+        RgbaImage { data }
+    }
+
+    #[wasm_bindgen(js_name = "imageData")]
+    pub fn image_data(&self) -> ImageData {
+        let mut data = Array1::zeros(self.data.len() * 4);
+        self.data
+            .as_slice()
+            .unwrap()
+            .iter()
+            .enumerate()
+            .for_each(|(i, val)| {
+                data.slice_mut(s![i * 4..(i + 1) * 4])
+                    .zip_mut_with(&Array1::from_iter(val), |a, b| *a = **b);
+            });
+        ImageData::new_with_u8_clamped_array(
+            Clamped(data.as_slice().unwrap()),
+            self.data.dim().1 as u32,
+        )
+        .unwrap()
+    }
+}
 
 #[wasm_bindgen]
 pub struct Perlin {
-    inner: crate::perlin::Perlin<f32>
+    inner: crate::perlin::Perlin<f32>,
 }
 
 #[wasm_bindgen]
 impl Perlin {
-
     #[wasm_bindgen(constructor)]
     pub fn new(scale: usize, amp: f32) -> Perlin {
-        Perlin { inner: crate::perlin::Perlin::<f32>::new(scale, amp) }
+        Perlin {
+            inner: crate::perlin::Perlin::<f32>::new(scale, amp),
+        }
     }
 
-
-    #[wasm_bindgen(js_name="addToImage")]
+    #[wasm_bindgen(js_name = "addToImage")]
     pub fn add_to_image(&self, image: &mut ScalarImage) {
         self.inner.add_to_image(&mut image.data)
     }
--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,34 +1,14 @@
-import imggen, { ScalarImage, Perlin } from "imtools";
+import imggen, { Perlin, RgbaImage } from "imtools";
 
 await imggen();
 
 const WIDTH = 1000;
 const HEIGHT = 500;
-const imageGenerator = new ScalarImage(WIDTH, HEIGHT);
-
-new Perlin(200, 1.1).addToImage(imageGenerator);
-new Perlin(140, 0.1).addToImage(imageGenerator);
-new Perlin(90, 0.1).addToImage(imageGenerator);
-new Perlin(70, 0.1).addToImage(imageGenerator);
-new Perlin(40, 0.04).addToImage(imageGenerator);
-new Perlin(10, 0.02).addToImage(imageGenerator);
-
+const imageGenerator = RgbaImage.fill(WIDTH, HEIGHT, 200, 100, 0);
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const imageData = ctx.createImageData(WIDTH, HEIGHT);
-
-let i = 0;
-imageGenerator.imageData().forEach(element => {
-    const level = Math.max(Math.min(element + 0.5, 1.0), 0.0) * 255.0;
-    imageData.data[i] = level;
-    imageData.data[i + 1] = level;
-    imageData.data[i + 2] = level;
-    imageData.data[i + 3] = 255;
-    i = i + 4;
-});
-
-ctx.putImageData(imageData, 0, 0);
+ctx.putImageData(imageGenerator.imageData(), 0, 0);
