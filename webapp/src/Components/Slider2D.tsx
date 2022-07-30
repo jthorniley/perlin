bf578dc48c76da9756f68f53cf320b81b1954124
James Thorniley <james.thorniley@gmail.com>
2022-07-30 09:56:34 +0100

    New slider impl

--- webapp/src/Components/Slider2D.tsx
+++ webapp/src/Components/Slider2D.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { MouseEventHandler } from "react"
 
 const PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
     61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139,
@@ -6,7 +6,102 @@ const PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
     233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
     331, 337, 347, 349, 353, 359]
 
+const CHART_BACKGROUND = "#86198f";
+const HIGHTLIGHT = "#fae8ff";
+
+export type Limits = { xMin: number, xMax: number, yMin: number, yMax: number }
+
+class Transform {
+    private _xDataRange: number
+    private _yDataRange: number
+    private _xScreenRange: number
+    private _yScreenRange: number
+
+    constructor(private dataLimits: Limits, private screenLimits: Limits) {
+        this._xDataRange = dataLimits.xMax - dataLimits.xMin
+        this._yDataRange = dataLimits.yMax - dataLimits.yMin
+        this._xScreenRange = screenLimits.xMax - screenLimits.xMin
+        this._yScreenRange = screenLimits.yMax - screenLimits.yMin
+    }
+
+    get xScreenRange(): number {
+        return this._xScreenRange
+    }
+
+    get yScreenRange(): number {
+        return this._yScreenRange
+    }
+
+    toDataX(value: number): number {
+        const x = this.dataLimits.xMin + this._xDataRange * (value - this.screenLimits.xMin) / this._xScreenRange
+        return Math.min(this.dataLimits.xMax, Math.max(this.dataLimits.xMin, x))
+    }
+
+    toDataY(value: number): number {
+        const y = this.dataLimits.yMin + this._yDataRange * (value - this.screenLimits.yMin) / this._yScreenRange
+        return Math.min(this.dataLimits.yMax, Math.max(this.dataLimits.yMin, y))
+    }
+
+    toScreenX(value: number): number {
+        const x = this.screenLimits.xMin + this._xScreenRange * (value - this.dataLimits.xMin) / this._xDataRange
+        return Math.min(this.screenLimits.xMax, Math.max(this.screenLimits.xMin, x))
+    }
+
+    toScreenY(value: number): number {
+        const y = this.screenLimits.yMin + this._yScreenRange * (value - this.dataLimits.yMin) / this._yDataRange
+        return Math.max(this.screenLimits.yMax, Math.min(this.screenLimits.yMin, y))
+    }
+}
+
 export type Slider2DProps = {
+    value: [number, number]
+    setValue: (value: [number, number]) => void
+    dataLimits?: Limits
+    screenLimits?: Limits
+}
+
+export function Slider2D(props: Slider2DProps) {
+    const { value, setValue, dataLimits, screenLimits } = props;
+
+    const transform = React.useMemo(() => {
+        return new Transform(
+            dataLimits ?? { xMin: 2, xMax: 200, yMin: 0, yMax: 1 },
+            screenLimits ?? { xMin: 10, xMax: 190, yMin: 190, yMax: 10 }
+        )
+    }, [dataLimits, screenLimits])
+
+    const screenPosition = [transform.toScreenX(value[0]), transform.toScreenY(value[1])];
+
+    const onMouse = React.useCallback<MouseEventHandler>((ev) => {
+        const { left, top } = ev.currentTarget.getBoundingClientRect();
+        const [screenX, screenY] = [
+            ev.clientX - left,
+            ev.clientY - top
+        ]
+
+        setValue([
+            transform.toDataX(screenX),
+            transform.toDataY(screenY)
+        ])
+    }, [transform, setValue])
+
+    return (<div className="h-full w-full">
+        <svg style={{ width: "100%", height: "100%" }}
+            onMouseMove={onMouse}
+        >
+            <rect
+                fill={CHART_BACKGROUND}
+                x={transform.toScreenX(0)} y={transform.toScreenY(1)}
+                width={transform.xScreenRange} height={-transform.yScreenRange}
+            />
+
+            <circle cx={screenPosition[0]} cy={screenPosition[1]} r="5" stroke="white" fill="transparent" />
+            <circle cx={screenPosition[0]} cy={screenPosition[1]} r="1" fill="white" />
+        </svg>
+    </div>)
+}
+
+export type Slider2DXProps = {
     scale: number,
     setScale: (_: number) => void
     amp: number,
@@ -15,7 +110,7 @@ export type Slider2DProps = {
     chartOffset?: [number, number]
 }
 
-export function Slider2D(props: Slider2DProps) {
+export function Slider2DX(props: Slider2DXProps) {
     const { scale, setScale, amp, setAmp, shape, chartOffset } = props;
     const [width, height] = shape ?? [200, 200];
     const [chartOffsetX, chartOffsetY] = chartOffset ?? [10, 10];
--- webapp/src/Components/Slider2DDemo.tsx
+++ webapp/src/Components/Slider2DDemo.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Slider2D } from "./Slider2D";
+import { Slider2D, Slider2DX } from "./Slider2D";
 
 export function Slider2DDemo() {
-    const [scale, setScale] = React.useState(17)
-    const [amp, setAmp] = React.useState(0.5);
-
+    const [value, setValue] = React.useState<[number, number]>([200, 1]);
     return (
-        <div className="flex flex-col">
-            <Slider2D scale={scale} setScale={setScale} amp={amp} setAmp={setAmp} />
-            <div className="text-fuchsia-100">Scale: {scale}</div>
-            <div className="text-fuchsia-100">Amp: {amp.toFixed(3)}</div>
+        <div className=" border-lime-400 border-2 m-6 p-6" style={{ width: 250, height: 250 }}>
+            <Slider2D screenLimits={{ xMin: 40, xMax: 190, yMin: 190, yMax: 30 }} {...{ value, setValue }} />
         </div>
     )
+    /*    return (
+            <div className="flex flex-col">
+                <Slider2DX scale={scale} setScale={setScale} amp={amp} setAmp={setAmp} />
+                <div className="text-fuchsia-100">Scale: {scale}</div>
+                <div className="text-fuchsia-100">Amp: {amp.toFixed(3)}</div>
+            </div>
+        )*/
 }
