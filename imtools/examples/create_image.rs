ea8ab7624e57b72c4627006966218d53609a8892
James Thorniley <james.thorniley@gmail.com>
2022-07-31 11:19:10 +0100

    Grassy example

--- imtools/examples/create_image.rs
+++ imtools/examples/create_image.rs
@@ -3,21 +3,29 @@ use std::error::Error;
 use image::*;
 use imtools::prelude::*;
 use ndarray::prelude::*;
-use palette::gradient::named::MAGMA;
+use palette::convert::TryIntoColor;
+
+use palette::gradient::Gradient;
+use palette::{Hsl, Srgb};
 
 pub fn main() -> Result<(), Box<dyn Error>> {
+    let colors: Vec<Srgb> = vec![
+        Hsl::new(85.0, 0.2, 0.5).try_into_color().unwrap(),
+        Hsl::new(75.0, 0.3, 0.5).try_into_color().unwrap(),
+        Hsl::new(65.0, 0.3, 0.7).try_into_color().unwrap(),
+    ];
+    let grad = Gradient::new(colors.into_iter().map(|col| col.into_linear()));
     let cols = 1200;
     let rows = 1000;
 
     let mut img: Array2<f32> = Array::zeros([rows, cols]);
 
-    Perlin::new(400, 0.9).add_to_image(&mut img);
-    Perlin::new(158, 0.3).add_to_image(&mut img);
+    Perlin::new(400, 0.6).add_to_image(&mut img);
+    Perlin::new(158, 0.5).add_to_image(&mut img);
     Perlin::new(101, 0.3).add_to_image(&mut img);
-    Perlin::new(59, 0.1).add_to_image(&mut img);
-    Perlin::new(3, 0.1).add_to_image(&mut img);
+    Perlin::new(59, 0.2).add_to_image(&mut img);
 
-    let result = GradientCMap::new(MAGMA).cmap(&img);
+    let result = GradientCMap::new(grad).cmap(&img);
 
     let image_buffer =
         RgbaImage::from_raw(cols as u32, rows as u32, result.as_flat_slice().to_vec()).unwrap();
