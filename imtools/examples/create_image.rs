26e21cff8f4c7e44828e621274fc0cd3f794acc8
James Thorniley <james.thorniley@gmail.com>
2022-07-31 16:26:53 +0100

    Grass

--- imtools/examples/create_image.rs
+++ imtools/examples/create_image.rs
@@ -10,9 +10,9 @@ use palette::{Hsl, Srgb};
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     let colors: Vec<Srgb> = vec![
-        Hsl::new(85.0, 0.2, 0.5).try_into_color().unwrap(),
-        Hsl::new(75.0, 0.3, 0.5).try_into_color().unwrap(),
-        Hsl::new(65.0, 0.3, 0.7).try_into_color().unwrap(),
+        Hsl::new(80.0, 0.15, 0.5).try_into_color().unwrap(),
+        Hsl::new(75.0, 0.2, 0.55).try_into_color().unwrap(),
+        Hsl::new(70.0, 0.3, 0.6).try_into_color().unwrap(),
     ];
     let grad = Gradient::new(colors.into_iter().map(|col| col.into_linear()));
     let cols = 1200;
@@ -20,10 +20,10 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 
     let mut img: Array2<f32> = Array::zeros([rows, cols]);
 
-    Perlin::new(400, 0.6).add_to_image(&mut img);
-    Perlin::new(158, 0.5).add_to_image(&mut img);
-    Perlin::new(101, 0.3).add_to_image(&mut img);
-    Perlin::new(59, 0.2).add_to_image(&mut img);
+    Perlin::new(400, 0.8).add_to_image(&mut img);
+    Perlin::new(158, 0.2).add_to_image(&mut img);
+    Perlin::new(101, 0.1).add_to_image(&mut img);
+    Perlin::new(59, 0.01).add_to_image(&mut img);
 
     let result = GradientCMap::new(grad).cmap(&img);
 
